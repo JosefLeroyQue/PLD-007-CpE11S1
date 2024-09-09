1ce0b068c1d770b2a1c659fdb8a6b67a73c49d37
JosefLeroyQue <qjlpque@tip.edu.ph>
2024-09-09 12:38:18 +0800

    Add files via upload

---
+++ Algorithm to get the sum,difference,product,quotient, and remainder of two integers.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+ using namespace std;
+
+int main() 
+{
+  int x=4,y=2;
+  int sum,difference,product,remainder;
+  float quotient;
+
+  sum=x+y;
+  difference=x-y;
+  product=x*y;
+  quotient=x/y;
+  remainder=x%y;
+
+  cout<<"Sum:"<<sum<<endl;
+  cout<<"Difference:"<<difference<<endl;
+  cout<<"Product:"<<product<<endl;
+  cout<<"Qoutient:"<<quotient<<endl;
+  cout<<"Remainder:"<<remainder<<endl;
+  
+  return 0;
+  
+}
