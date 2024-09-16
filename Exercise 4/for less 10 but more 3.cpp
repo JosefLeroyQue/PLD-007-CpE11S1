be858af3807d891b8c17043a8b0c19a99a637903
JosefLeroyQue <qjlpque@tip.edu.ph>
2024-09-16 11:56:43 +0800

    Add files via upload

---
+++ Exercise 4/for less 10 but more 3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i=4;
+	int j=9;
+	
+	cout<<"C++ program that displays integers less than 10 but more than 3"<<endl;
+	for(;i<=j;i++)
+	{
+			cout<<"Integer:"<<i<<endl;
+	}
+	return 0;
+}
