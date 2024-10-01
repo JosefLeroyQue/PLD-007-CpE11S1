f2f5f3407da147a917f05b8f534fc466b394d29d
JosefLeroyQue <qjlpque@tip.edu.ph>
2024-10-01 16:59:39 +0800

    Add files via upload

---
+++ Hands on Activity 1.3: Writing  First Program using C Language/Exercise 1.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name: QUE,JOSEF LEROY P.\n");
+    printf("Course and Section: CPE11S1\n");
+    return 0;
+}
---
+++ Hands on Activity 1.3: Writing  First Program using C Language/Exercise 2.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("QUE,JOSEF LEROY P.\n");
+    printf("QUE,JOSEF LEROY P.\n");
+    printf("QUE,JOSEF LEROY P.\n");
+    return 0;
+}
