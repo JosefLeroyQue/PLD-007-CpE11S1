f934cecffa9d98220452cb92c00f7863f7325648
JosefLeroyQue <qjlpque@tip.edu.ph>
2024-10-02 12:51:43 +0800

    Add files via upload

---
+++ Practical Lab.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	// Practical exam number 3
+	int a=-5;
+	int b=4;
+	int sum;
+	
+	cout<<"A C++ program that will produce an output of sum of 10 numbers less than 5 using For loop"<<endl;
+	cout<<"First integer: -5"<<endl;
+	cout<<"Last integer: 4"<<endl;
+	for(;a<=b;a++);
+	{
+		sum = a;
+		cout<<"Sum: "<<a<<endl;
+	}
+	return 0;
+}
+
